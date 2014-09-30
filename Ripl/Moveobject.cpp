01c5725846059a5699e291f8bc57ad771568e769
Stanley Quay <squay@student.unimelb.edu.au>
2014-09-30 20:05:28 +1000

    Moveobject file made
    trying to fix error:name followed by ":: must be a class of namespace
    name

---
+++ Ripl/Moveobject.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "Moveobject.h"
+
+using namespace Ripl;
+using namespace DirectX;
+using namespace Windows::Foundation;
+
+Moveobject::Moveobject{};
---
+++ Ripl/Moveobject.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "pch.h"
+#include "Structures\VertexStructures.h"
+#include "Structures\ShaderStructures.h"
