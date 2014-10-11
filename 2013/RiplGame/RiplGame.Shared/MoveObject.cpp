447b505e9c9d6f9a7880d7c466f5b3557ef6648c
Stanley Quay <squay@student.unimelb.edu.au>
2014-10-12 01:08:14 +1100

    framework classes

---
+++ 2013/RiplGame/RiplGame.Shared/MoveObject.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "MoveObject.h"
+#include "Content\CommonFunctions.h"
+
+using namespace RiplGame;
+using namespace DirectX;
+using namespace Windows::Foundation;
+
+MoveObject::MoveObject(XMFLOAT3 coord, unsigned short sideLengthZ, unsigned short sideLengthX, unsigned short sideLengthY) {
+	
+}
+
+void MoveObject::fillVertices(unsigned short sideLengthZ, unsigned short sideLengthX, unsigned short sideLengthY, XMFLOAT3 colour)
+{
+
+}
---
+++ 2013/RiplGame/RiplGame.Shared/MoveObject.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "pch.h"
+#include "Content\Structures\VertexStructures.h"
+#include "Content\Structures\ShaderStructures.h"
+
+namespace RiplGame
+{
+	class MoveObject
+	{
+	public:
+		MoveObject::MoveObject(XMFLOAT3 coord, unsigned short sideLengthZ, unsigned short sideLengthX, unsigned short sideLengthY);
+		unsigned short* indices;
+
+	private:
+		XMFLOAT4 defaultColour;
+
+		// Direct3D resources for cube geometry.
+		Microsoft::WRL::ComPtr<ID3D11InputLayout>	inputLayout;
+		Microsoft::WRL::ComPtr<ID3D11VertexShader>	vertexShader;
+		Microsoft::WRL::ComPtr<ID3D11PixelShader>	pixelShader;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>		constantBuffer;
+
+		// System resources for cube geometry.
+		ShaderCBuffer	constantBufferData;
+		uint32	indexCount;
+		uint32	vertexCount;
+		void MoveObject::fillVertices(unsigned short sideLengthZ, unsigned short sideLengthX,  unsigned short sideLengthY, XMFLOAT3 colour);
+
+	};
+}
