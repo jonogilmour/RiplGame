352de194d569d69e9ceb8423c2d9f2f4909a5869
Stanley Quay <squay@student.unimelb.edu.au>
2014-10-11 16:54:23 +1100

    Revert "Revert "created moving object classes""
    This reverts commit cd823345fe14cbe9d80fb4d969a6d98a9a17c474.

---
+++ 2013/RiplGame/RiplGame.Shared/MovingObject.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "MovingObject.h"
+#include "Content\CommonFunctions.h"
+
+using namespace RiplGame;
+using namespace DirectX;
+using namespace Windows::Foundation;
+
+MovingObject::MovingObject(XMFLOAT3 Position, unsigned short sideLengthZ, unsigned short sideLengthX,unsigned short sideLengthY)
+{
+
+}
+void fillVertices(unsigned short sideLengthZ, unsigned short sideLengthX, XMFLOAT3 colour)
+{
+}
+void fillIndices(unsigned short sideLengthZ, unsigned short sideLengthX)
+{
+
+}
