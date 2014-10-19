78abe225705c21234ec7c698be99a8acdc91328d
Stanley Quay <squay@student.unimelb.edu.au>
2014-10-20 02:40:27 +1100

    created ray class

---
+++ 2013/RiplGame/RiplGame.Shared/Ray.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "Ray.h"
+#include "Content\CommonFunctions.h"
+Ray::Ray()
+{
+	XMFLOAT3 position;
+	XMFLOAT3 direction;
+}
---
+++ 2013/RiplGame/RiplGame.Shared/Ray.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "pch.h"
+#include "Content\Structures\VertexStructures.h"
+#include "Content\Structures\ShaderStructures.h"
+namespace RiplGame
+{
+	class Ray
+	{
+	public:
+		Ray();
+
+	private:
+	};
+}
