09a80f402fa6587b65e493e747fb407de3d13ff6
michaeman <mliuhaoo@gmail.com>
2014-10-22 16:46:46 +1100

    rippleMove
    Currently containing errors. Bits and pieces defined, no idea why
    XMFLOAT4 is not working.

--- 2013/RiplGame/RiplGame.Shared/RippleMove.cpp
+++ 2013/RiplGame/RiplGame.Shared/RippleMove.cpp
@@ -1,14 +1,30 @@
 #include "pch.h"
 #include "Content\CommonFunctions.h"
 #include "Collision.h"
+
 //The functions for moving the hero character when a tap near the hero is performed
+
+
+// check collision
+//direction vector for first tap,
+//direcion vector for second tap,
+//cross product to find diagonal vector
+//static xmfloat4 previous vector 0.0,0.0,0.0
+//after each tap, save the new vector to the previous vector.
+//if previousvector == null, (then it is first tap)
 
 
 //Center1 is center of the tap, center2 is the center of the hero.
 //radius 1 is radius of the tap (animation), radius 2 is the radius of the hero.
-float calcVelocity(XMFLOAT3 center1, float radius1, XMFLOAT3* center2, float radius2)
+XMFLOAT4 moveCharacter(XMFLOAT3 center1, float radius1, XMFLOAT3* center2, float radius2, float startingVelocity)
 {
+
+	static XMFLOAT4 prevVector = (0.0f, 0.0f, 0.0f, 0.0f);
 	//The speed and max_distance will need testing to make it perfect.
+	float x;
+	float y;
+	float z;
+
 	float speed = 1;
 	float max_distance = 10;
 
@@ -16,17 +32,28 @@ float calcVelocity(XMFLOAT3 center1, float radius1, XMFLOAT3* center2, float rad
 
 	float velocity = speed * (max_distance - dist);
 
+	//spheresphereCollision(XMFLOAT3* center1, float r1, XMFLOAT3 center2, float r2)
+
+	if !(spheresphereCollision(tapPosition,RippleRadius,characterPosition,characterRadius)){
+		return (x,y,z,0);
+	}
+
+	if (startingVelocity != 0)
+	{
+		// it is second tap
+
+	}
 	//This makes the spot to tap that makes the highest 'speed' is when tapping just outside the heros radius region.
 	if (velocity > 0 && dist > (radius1 + radius2))
 	{
-		return velocity;
+		return (x,y,z,velocity);
 	}
 
 	else //velocity is negative number, for being:
 		//too far away, the dist is > max_distance or
 		//the tap was too close, it is inside on the hero, the dist is a negative number.
 	{
-		return 0;
+		return (x,y,z,0.0f);
 	}
 }
 
--- 2013/RiplGame/RiplGame.Shared/RippleMove.h
+++ 2013/RiplGame/RiplGame.Shared/RippleMove.h
@@ -1,3 +1,3 @@
 #include "pch.h"
 
-float calcVelocity(XMFLOAT3 center1, float radius1, XMFLOAT3* center2, float radius2);
+float moveCharacter(XMFLOAT3 center1, float radius1, XMFLOAT3* center2, float radius2);
